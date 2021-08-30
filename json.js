1bcae646badb98afe5145aaa318cfbcb81db0839
Rony Miah <ronymiah2121@gmail.com>
2021-08-30 05:57:57 -0600

    User name and Email crate

--- json.js
+++ json.js
@@ -1,1 +1,33 @@
-console.log('Linked SucessFull');
+// function loadUsers() {
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//         .then(res => res.json())
+//         .then(data => displyUsers(data));
+// }
+
+
+
+
+
+
+function loadUsers() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => displyUsers(data));
+}
+
+function loadPhotos() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(data => console.log(data));
+}
+
+function displyUsers(data) {
+    const ul = document.getElementById('users');
+    for (const user of data) {
+        const li = document.createElement('li');
+        li.innerText = `name ${user.name} email : ${user.email}`;
+        ul.appendChild(li);
+
+    }
+
+}
